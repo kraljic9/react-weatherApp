a9db973bc6d88a4aa311a38a9e5e4f7296d17533
Pavle Kraljic <kraljicpavle9@gmail.com>
2026-02-24 16:02:45 +0100

    Added a display weather container which will include two parts a today forcast and a daily forcast

--- weather-app/src/Components/DisplayWeather.jsx
+++ weather-app/src/Components/DisplayWeather.jsx
@@ -1,5 +1,38 @@
+import { useContext } from "react";
+import { WeatherContext } from "../Context/WeatherContext";
+
 function DispalyWeather() {
-  return <h1>Weather Display</h1>;
+  const { weatherData } = useContext(WeatherContext);
+
+  console.log(weatherData);
+
+  const dailyFilter = weatherData.list.filter((weather) =>
+    weather.dt_txt.includes("12:00:00"),
+  );
+
+  console.log(dailyFilter);
+
+  return (
+    <>
+      {weatherData ? (
+        <div className="display-weather-container">
+          <div className="today-forcast">
+            <img src="" alt="" className="today-weather-img" />
+            <div className="today-weather-data">
+              <p>Today</p>
+              <p className="city-name">{weatherData.city.name}</p>
+              <p>Temperature: {weatherData.list[0].main.temp}°C</p>
+              <p>{weatherData.list[0].weather[0].description}</p>
+            </div>
+
+            <div className="daily-forcast"></div>
+          </div>
+        </div>
+      ) : (
+        false
+      )}
+    </>
+  );
 }
 
 export default DispalyWeather;
