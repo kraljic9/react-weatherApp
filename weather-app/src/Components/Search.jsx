467fae70f44ac56fd3bfb3fa17ff9589ec7884da
Pavle Kraljic <kraljicpavle9@gmail.com>
2026-02-24 15:41:50 +0100

    Added city state, handleSearch function and used weather context

--- weather-app/src/Components/Search.jsx
+++ weather-app/src/Components/Search.jsx
@@ -1,7 +1,20 @@
+import { useContext, useEffect, useState } from "react";
+import { WeatherContext } from "../Context/WeatherContext";
+
 function Search() {
+  const { loading, error, weatherData, fetchWeather } =
+    useContext(WeatherContext);
+
+  const [city, setCity] = useState("");
+
+  function handleSearc() {
+    fetchWeather(city);
+  }
+
   return (
     <>
-      <input type="text" name="" id="" />
+      <input type="text" onChange={(e) => setCity(e.target.value)} />
+      <button onClick={handleSearc}>Search</button>
     </>
   );
 }
