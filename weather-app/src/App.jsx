47452616156f4bde0c0a1395302d3c00daea0824
Pavle Kraljic <kraljicpavle9@gmail.com>
2026-02-24 15:40:11 +0100

    Organized componants

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -1,14 +1,13 @@
+import { useContext } from "react";
 import "./App.css";
 import DispalyWeather from "./Components/DisplayWeather";
 import Search from "./Components/Search";
 import ContextProvider from "./Context/ContextProvider";
+import { WeatherContext } from "./Context/WeatherContext";
 
 function App() {
   return (
     <>
-      <Search />
-      <DispalyWeather />
-
       <ContextProvider>
         <DispalyWeather />
         <Search />
