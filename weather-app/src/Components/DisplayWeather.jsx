53fa80d1f85a8a4f74edae9741ec1826306b144b
Pavle Kraljic <kraljicpavle9@gmail.com>
2026-02-24 16:20:45 +0100

    Added guard for weather data

--- weather-app/src/Components/DisplayWeather.jsx
+++ weather-app/src/Components/DisplayWeather.jsx
@@ -4,6 +4,8 @@ import { WeatherContext } from "../Context/WeatherContext";
 function DispalyWeather() {
   const { weatherData } = useContext(WeatherContext);
 
+  if (!weatherData) return null;
+
   console.log(weatherData);
 
   const dailyFilter = weatherData.list.filter((weather) =>
@@ -12,25 +14,35 @@ function DispalyWeather() {
 
   console.log(dailyFilter);
 
+  let date = new Date("2026-03-01 12:00:00");
+
+  console.log(date.getDay());
+
   return (
     <>
-      {weatherData ? (
-        <div className="display-weather-container">
-          <div className="today-forcast">
-            <img src="" alt="" className="today-weather-img" />
-            <div className="today-weather-data">
-              <p>Today</p>
-              <p className="city-name">{weatherData.city.name}</p>
-              <p>Temperature: {weatherData.list[0].main.temp}°C</p>
-              <p>{weatherData.list[0].weather[0].description}</p>
-            </div>
+      <div className="display-weather-container">
+        <div className="today-forcast">
+          <img src="" alt="" className="today-weather-img" />
+          <div className="today-weather-data">
+            <p>Today</p>
+            <p className="city-name">{weatherData.city.name}</p>
+            <p>Temperature: {weatherData.list[0].main.temp}°C</p>
+            <p>{weatherData.list[0].weather[0].description}</p>
+          </div>
 
-            <div className="daily-forcast"></div>
+          <div className="daily-forcast">
+            {dailyFilter.map((weather) => (
+              <div className="daily-weather-box" key={weather.dt}>
+                <p className="day"></p>
+                <img src="" alt="" className="daily-weather-img" />
+                <div className="daily-weather-temperature">
+                  {weather.main.temp}°C
+                </div>
+              </div>
+            ))}
           </div>
         </div>
-      ) : (
-        false
-      )}
+      </div>
     </>
   );
 }
