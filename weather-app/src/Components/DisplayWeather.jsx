6d2659011674cbd5f655f650acdd8f767881c262
Pavle Kraljic <kraljicpavle9@gmail.com>
2026-02-25 11:13:20 +0100

    Added days array that helps display the daily weather days

--- weather-app/src/Components/DisplayWeather.jsx
+++ weather-app/src/Components/DisplayWeather.jsx
@@ -12,38 +12,44 @@ function DispalyWeather() {
     weather.dt_txt.includes("12:00:00"),
   );
 
-  console.log(dailyFilter);
-
-  let date = new Date("2026-03-01 12:00:00");
-
-  console.log(date.getDay());
+  let days = [
+    "sunday",
+    "monday",
+    "tuesday",
+    "wednesday",
+    "thursday",
+    "friday",
+    "saturday",
+  ];
 
   return (
-    <>
-      <div className="display-weather-container">
-        <div className="today-forcast">
-          <img src="" alt="" className="today-weather-img" />
-          <div className="today-weather-data">
-            <p>Today</p>
-            <p className="city-name">{weatherData.city.name}</p>
-            <p>Temperature: {weatherData.list[0].main.temp}°C</p>
-            <p>{weatherData.list[0].weather[0].description}</p>
-          </div>
-
-          <div className="daily-forcast">
-            {dailyFilter.map((weather) => (
-              <div className="daily-weather-box" key={weather.dt}>
-                <p className="day"></p>
-                <img src="" alt="" className="daily-weather-img" />
-                <div className="daily-weather-temperature">
-                  {weather.main.temp}°C
-                </div>
-              </div>
-            ))}
-          </div>
+    <div className="display-weather-container">
+      <div className="today-forcast">
+        <img src="" alt="" className="today-weather-img" />
+        <div className="today-weather-data">
+          <p>Today</p>
+          <p className="city-name">{weatherData.city.name}</p>
+          <p>Temperature: {weatherData.list[0].main.temp}°C</p>
+          <p>{weatherData.list[0].weather[0].description}</p>
         </div>
       </div>
-    </>
+
+      <div className="daily-forcast">
+        {dailyFilter.map((weather) => {
+          const day = new Date(weather.dt_txt);
+
+          return (
+            <div className="daily-weather-box" key={weather.dt}>
+              <p className="day">{days[day.getDay()]}</p>
+              <img src="" alt="" className="daily-weather-img" />
+              <div className="daily-weather-temperature">
+                {weather.main.temp}°C
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
   );
 }
 
